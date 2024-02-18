9eec87ae7d217793a7ce3b757fd82af66a9bc44c
Bogdan <bogdan@localhost.localdomain>
2024-02-18 21:08:33 +0200

    fix/lsof

--- flask/flask3.py
+++ flask/flask3.py
@@ -3,9 +3,9 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/<name>")
 #function of page
-def home():
+def home(name):
     return render_template("index.html")
 
 if __name__ == "__main__":
